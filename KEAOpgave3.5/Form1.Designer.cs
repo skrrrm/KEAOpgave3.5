5e9d050c0ca7382a00571fdb5a0f9b135cb94e6a
skrrrm <a@macbook-windows>
2020-02-21 11:35:18 +0100

    Add project files.

---
+++ KEAOpgave3.5/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace KEAOpgave3._5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.labelRes = new System.Windows.Forms.Label();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(156, 89);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(0, 25);
+            this.labelInfo.TabIndex = 0;
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Location = new System.Drawing.Point(391, 89);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(100, 31);
+            this.textBoxNumber.TabIndex = 1;
+            // 
+            // labelRes
+            // 
+            this.labelRes.AutoSize = true;
+            this.labelRes.Location = new System.Drawing.Point(156, 216);
+            this.labelRes.Name = "labelRes";
+            this.labelRes.Size = new System.Drawing.Size(0, 25);
+            this.labelRes.TabIndex = 2;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(391, 216);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(110, 39);
+            this.buttonCalculate.TabIndex = 3;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.labelRes);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.labelInfo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.Label labelRes;
+        private System.Windows.Forms.Button buttonCalculate;
+    }
+}
+
---
+++ KEAOpgave3.5/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Udarbejd et lille program, hvor brugeren får besked på om et indtastet tal ligger mellem 10 og 20
+
+namespace KEAOpgave3._5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            labelInfo.Text = "Write a number between 10 and 20.";
+        }
+
+        // Variables 
+        double result;
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                result = Convert.ToDouble(textBoxNumber.Text); // Converts to double
+            }
+            catch
+            {
+                labelInfo.Text = "Numbers only"; // error 
+                
+            }
+
+            if (result >= 10 && result <= 20)
+                labelRes.Text = "Correct!";
+            else
+                labelRes.Text = "Incorrect!";
+        }
+    }
+}
